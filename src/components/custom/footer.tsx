5d52fb7694e38c7106b00d79b40b378e9eda1a0e
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-08-19 23:36:16 +0500

    footer and heading.

---
+++ src/components/custom/footer.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <div className="bg-primary container">
+      <div className="container text-white flex flex-col">
+        <div className="rights text-center">
+          © 2024 newsaggregator | All Rights Reserved
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
